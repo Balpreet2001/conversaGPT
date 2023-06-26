46363f48fa4857f682e63902790f5988dea2212a
Balpreet2001 <balpreetsingh8845@gmail.com>
2023-06-27 01:28:55 +0530

    created basic POST api which takes prompt and sends response

--- conversegpt/models.py
+++ conversegpt/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Prompt(models.Model):
+    prompt_text = models.CharField(max_length=255)
+
+class Result(models.Model):
+    prompt = models.ForeignKey(Prompt, on_delete=models.CASCADE)
+    result_text = models.TextField()
--- conversegpt/serializers.py
+++ conversegpt/serializers.py
@@ -1,1 +1,12 @@
-from rest_framework.serializers import Serializer
+from rest_framework import serializers
+from .models import Prompt, Result
+
+class PromptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prompt
+        fields = '__all__'
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = '__all__'
